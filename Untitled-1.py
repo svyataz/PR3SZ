9177c742d9a5ab91f69044c73757c4d942f77022
svyataz <svyataz@gmail.com>
2024-10-16 19:00:00 +0300

    nice

--- Untitled-1.py
+++ Untitled-1.py
@@ -3,24 +3,52 @@ import pandas as pd
 
 class inspection :
     def __init__(self):
+        self.__answers = []
         self.__ddb = pd.read_csv("C:/Users/svyat/Desktop/тестирование/PR3SZ/main.csv")
     
-    def startInspection (self):
-        while(len(self.__ddb.columns) > 1 and len(self.__ddb) > 1):
-            self.__s = self.__ddb.head(1).transpose().loc[self.__ddb.iloc[0] == 1].index[0]
-            print("do you have", self.__s[self.__s.find('_') + 1:], "?(y/n):", end="")
-            ans = input()
-            if not(ans == 'y' or ans == 'n'):
-                print("incorrect answer")
-                continue
-            elif ans == 'y':
-                self.__ddb = self.__ddb[self.__ddb[self.__s] == 1]
-                if(self.__ddb.columns[-2] == self.__s):
-                    print("diagnosis:", self.__ddb.iloc[0]['label'][self.__s.find('_') + 1:])
-            else:
-                self.__ddb = self.__ddb[self.__ddb[self.__s] == 0]
+    def ddb_setter(self, newdb):
+        self.__ddb = newdb
+
+    def ddb_getter(self):
+        return  self.__ddb
+    
+    def ans_setter(self, newans):
+        self.__ans = newans
+
+    def ans_getter(self):
+        return  self.__ans
+    
+    def ansInput(self):
+        self.__s = self.__ddb.head(1).transpose().loc[self.__ddb.iloc[0] == 1].index[0]
+        print("do you have", self.__s[self.__s.find('_') + 1:], "?(y/n):", end="")
+        self.__ans = input()
+    
+    def ans_check(self):
+        if not(self.__ans == 'y' or self.__ans == 'n'):
+            print("incorrect answer")
+            return True
+        elif self.__ans == 'y':
+            self.__ddb = self.__ddb[self.__ddb[self.__s] == 1]
+        else:
+            self.__ddb = self.__ddb[self.__ddb[self.__s] == 0]
+    
+    def loop(self):
+        if (len(self.__ddb.columns) > 1 and len(self.__ddb) > 1):
+            self.ansInput()
+            if(self.ans_check()): 
+                self.startInspection()
+                return None
             self.__ddb = self.__ddb.drop(self.__s, axis=1)
-            print(self.__ddb)
-        print("diagnosis:", self.__ddb.iloc[0]['label'][self.__s.find('_') + 1:])
+            self.__answers.append(self.__ans)
+        else: return None
+        self.loop()
+   
+    def startInspection (self):
+        self.loop()
+        print(*self.__answers)
+        if not('y' in self.__answers):
+            return "You are healthy"
+        return "Diagnosis:" + self.__ddb.iloc[0]['label'][self.__s.find('_') + 1:]
+
 game_inst = inspection()
-game_inst.startInspection()
+print(game_inst.startInspection())
